dfc0e9772e1f3ee82ce9a364900a4bde61e15644
Jasonlee6789 <jeffersonlee@163.com>
2020-08-16 09:28:15 +0800

    I want to build a dialog element demo with vue.

---
+++ .history/js/dialog_20200814112840.js
@@ -0,0 +1,15 @@
+Vue.component("kkb-dialog", {
+  props: {},
+
+  template: `
+        <div class="dialog" v-show="visible">
+            <div class="dialog-header">
+                <span class="dialog-title">提示</span>
+                <i class="dialog-close" @click="close">x</i>
+            </div>
+            <div class="dialog-body">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+});
---
+++ .history/js/dialog_20200814113806.js
@@ -0,0 +1,19 @@
+Vue.component("kkb-dialog", {
+    props: {
+        title:{ '标题' },
+        visible: { false },
+    
+  },
+
+  template: `
+        <div class="dialog" v-show="visible">
+            <div class="dialog-header">
+                <span class="dialog-title">提示</span>
+                <i class="dialog-close" @click="close">x</i>
+            </div>
+            <div class="dialog-body">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+});
